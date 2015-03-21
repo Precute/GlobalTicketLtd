0a2a9fdebe803c2c10165d57ac3c31f90780fd58
Precious <precious_igbinosun@yahoo.co.uk>
2015-03-21 09:39:48 +0000

    bean
    a bean attraction catalogue

--- src/global/BeanAttractionCatalogue.java
+++ src/global/BeanAttractionCatalogue.java
@@ -23,6 +23,49 @@ public class BeanAttractionCatalogue implements Serializable{
 		attTypeID = aTypeID;
 		locationID = locID;
 	}
-
-
+	public int getAttractionID (){
+		return attractionID;
+	}
+	public void setAttractionID (int attractionID){
+		this.attractionID = attractionID;
+	}
+	public String getAttName (){
+		return attName;
+	}
+	public void setAttName(String attName){
+		this.attName = attName;
+	}
+	public String AttDescript (){
+		return attDescript;
+	}
+	public void setAttDescript(String attDescript){
+		this.attDescript = attDescript;
+	}
+	public int getAttAvailabilityCount(){
+		return attAvailabilityCount;	
+	}
+	public void setAttAvailabilityCount(int attAvailabilityCount){
+		this.attAvailabilityCount = attAvailabilityCount;
+	}
+	public int getAttTypeID(){
+		return attTypeID;
+	}
+	public void setAttTypeID(int attTypeID){
+		this.attTypeID = attTypeID;
+	}
+	public int getLocationID(){
+		return locationID;
+	}
+	public void setLocationID (int locationID){
+		this.locationID = locationID;
+	}
+	@Override
+	public String toString() {
+		return "BeanAttractionCatalogue [attractionID=" + attractionID
+				+ ", attName=" + attName + ", attDescript=" + attDescript
+				+ ", attAvailabilityCount=" + attAvailabilityCount
+				+ ", attTypeID=" + attTypeID + ", locationID=" + locationID
+				+ "]";
+	}
+	
 }
