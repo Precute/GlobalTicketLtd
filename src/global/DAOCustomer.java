5454c66af25ef1686d21cdecce809657d4c73e74
Precious <precious_igbinosun@yahoo.co.uk>
2015-03-24 10:33:57 +0000

    DAO for Employee and Customer

---
+++ src/global/DAOCustomer.java
@@ -0,0 +1,16 @@
+package global;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DAOCustomer {
+
+	public DAOCustomer() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/global/DAOEmployee.java
@@ -0,0 +1,16 @@
+package global;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DAOEmployee {
+
+	public DAOEmployee() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
